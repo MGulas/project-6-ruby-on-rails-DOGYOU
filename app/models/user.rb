b1bb55230114a0bc84c805e4fad30a208aea9563
Hunter Mills <ihunt0002@gmail.com>
2022-04-08 12:17:19 -0400

    baseline tests?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     belongs_to :team, :foreign_key => "team_number"
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email_address, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,24 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @team = Team.new(team_number: 999)
+    @user = User.new(email_address: "user@example.com", first_name: "Hunter", last_name: "Mills", team_number: 999)
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "first and last name should be present" do
+    @user.first_name = "     "
+    @user.last_name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email address should be present" do
+    @user.email_address = "     "
+    assert_not @user.valid?
+  end
+
 end
