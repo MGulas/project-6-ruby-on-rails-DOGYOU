106d3645b5825dad6841aec979d9bf6416da97a7
Hunter Mills <ihunt0002@gmail.com>
2022-04-08 12:17:28 -0400

    made it say hello world idk

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :comments
   resources :scores
   resources :users
+  root 'application#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
