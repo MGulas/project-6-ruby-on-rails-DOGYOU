4e5cc604f212a6c37d2997c5d8e8b38b4f080dd7
Hunter Mills <ihunt0002@gmail.com>
2022-04-11 13:04:52 -0400

    adding more validations and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,35 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "first name should not be too long" do
+    @user.first_name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "last name should not be too long" do
+    @user.last_name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email address should not be too long" do
+    @user.email_address = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email_address = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email_address = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
+
