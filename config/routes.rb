c553fd638d6430fdda520c4ed3927579a8ed5f12
Hunter Mills <ihunt0002@gmail.com>
2022-04-12 13:55:12 -0400

    logging in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   resources :scores
   resources :users
   root 'application#hello'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
