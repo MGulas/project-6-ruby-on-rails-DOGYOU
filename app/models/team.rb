e15265dbad7f07b995d9d958549637955cc3b2dd
Hunter Mills <ihunt0002@gmail.com>
2022-04-13 11:21:41 -0400

    making team numbers unique

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
     has_and_belongs_to_many :users, :foreign_key => "team_number"
     belongs_to :project
 
-    validates :team_number, presence: true
+    validates :team_number, presence: true, uniqueness: true
     validates :section_id, presence: true
     validates :team_name, presence: true
 end
