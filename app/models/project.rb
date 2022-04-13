80c7a834ce94d3df22ddb133b884d92f83277932
MGulas <michael.gulasy@gmail.com>
2022-04-13 14:53:00 -0400

    added foreign key for project to section

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,10 @@
 class Project < ApplicationRecord
-    belongs_to :course
+    belongs_to :course, :foreign_key => "section_id"
 
     has_many :scores
     has_many :comments
     has_many :teams
 
-    validates :p_number, presence: true
+    validates :p_number, presence: true, uniqueness: true
     validates :p_name, presence: true
 end
