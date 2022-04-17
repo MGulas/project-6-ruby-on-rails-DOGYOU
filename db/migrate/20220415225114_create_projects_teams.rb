8e475c448f6d58a5b1053d09f3dd7fb8593a4ccb
MGulas <michael.gulasy@gmail.com>
2022-04-17 01:23:05 -0400

    projects assigned to teams

--- db/migrate/20220415225114_create_projects_teams.rb
+++ db/migrate/20220415225114_create_projects_teams.rb
@@ -1,8 +1,8 @@
 class CreateProjectsTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :projects_teams do |t|
-
-      t.timestamps
+      t.belongs_to :project
+      t.belongs_to :team
     end
   end
 end
