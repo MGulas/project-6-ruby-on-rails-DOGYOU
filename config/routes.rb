1b08ca56c01bca248f33cc221b45c85eb87181f3
MGulas <michael.gulasy@gmail.com>
2022-04-18 01:28:12 -0400

    Added new page myprojects for user and a route to it.

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   post '/admin/add_user_to_team', to: 'admin#add_user_to_team'
   post '/admin/add_user_to_section', to: 'admin#add_user_to_section'
   post '/user/add_user_to_section', to: 'users#add_user_to_section'
+  get '/user/myprojects', to: 'users#myprojects'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
