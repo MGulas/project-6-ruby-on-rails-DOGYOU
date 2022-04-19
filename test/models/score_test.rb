e53463b593c284d7efc6f43474d8f59a4fc9cf1a
Hunter Mills <ihunt0002@gmail.com>
2022-04-19 10:48:28 -0400

    adding model tests

--- test/models/score_test.rb
+++ test/models/score_test.rb
@@ -4,4 +4,29 @@ class ScoreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @score = Score.new(creator: "me" , recipient: "you", project_id: 999, score: 1)
+    @project = Project.new(p_name: "test proj", p_number: 999)
+    @score.project = @project
+  end
+
+  test "should be valid" do
+    assert @score.valid?
+  end
+
+  test "creator should be present" do
+    @score.creator = "     "
+    assert_not @score.valid?
+  end
+
+  test "recipient should be present" do
+    @score.recipient = "     "
+    assert_not @score.valid?
+  end
+
+  test "project should be present" do
+    @score.project = nil
+    assert_not @score.valid?
+  end
 end
